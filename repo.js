13a54e3215d2b404c4e87b467330ec403d84bfcb
kuksa <kuksattu@gmail.com>
2022-09-09 11:49:06 +0300

    Formatting

--- repo.js
+++ repo.js
@@ -1,11 +1,11 @@
 {
-	"contact": "kuksattu@gmail.com, koponenkt#8073",
 	"id": "kuksa",
-	"patches": {
-		"NoPortraits": "Remove all anime girls from your game."
-	},
+	"title": "Kuksa's patch repository",
+	"contact": "kuksattu@gmail.com, koponenkt#8073",
 	"servers": [
 		"https://raw.githubusercontent.com/kuksattu/kuksa/master/"
 	],
-	"title": "Kuksa's patch repository"
+	"patches": {
+		"NoPortraits": "Remove all anime girls from your game."
+	}
 }
