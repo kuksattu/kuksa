db29252865e2f43c7ab6447db806fc4d39985f24
kuksa <kuksattu@gmail.com>
2022-09-09 10:16:51 +0300

    Idk anymore

--- NoPortraits/patch.js
+++ NoPortraits/patch.js
@@ -4,7 +4,8 @@
 	],
 	"id": "NoPortraits",
 	"servers": [
-		"https://mirrors.thpatch.net/kuksa/NoPortraits/"
+		// "https://mirrors.thpatch.net/kuksa/NoPortraits/"
+		"https://raw.githubusercontent.com/kuksattu/kuksa/master/NoPortraits/"
 	],
 	"title": "Remove all anime girls from your game."
 }
--- repo.js
+++ repo.js
@@ -3,7 +3,8 @@
     "id": "kuksa",
     "title": "Kuksa's patch repository",
     "servers": [
-        "https://mirrors.thpatch.net/kuksa/"
+        // "https://mirrors.thpatch.net/kuksa/"
+        "https://raw.githubusercontent.com/kuksattu/kuksa/master/"
     ],
     "patches": {
         "NoPortraits": "Remove all anime girls from your game."
