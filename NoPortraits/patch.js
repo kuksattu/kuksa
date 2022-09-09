143a91f1e5a160b9d8b90feeeb25c36b62693cdb
kuksa <kuksattu@gmail.com>
2022-09-09 09:33:02 +0300

    Updated repo.js and patch.js

--- NoPortraits/patch.js
+++ NoPortraits/patch.js
@@ -1,10 +1,10 @@
 {
-	// "dependencies": [
-	// 	"nmlgc/base_tsa"
-	// ],
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
 	"id": "NoPortraits",
 	"servers": [
 		"https://mirrors.thpatch.net/kuksa/NoPortraits/"
 	],
-	"title": "Removes all anime girls from your game."
+	"title": "Remove all anime girls from your game."
 }
--- repo.js
+++ repo.js
@@ -1,11 +1,11 @@
 {
     "contact": "kuksattu@gmail.com, koponenkt#8073",
     "id": "kuksa",
-    "title": "Kuksa's Repository",
+    "title": "Kuksa's patch repository",
     "servers": [
         "https://mirrors.thpatch.net/kuksa/"
     ],
     "patches": {
-        "NoPortraits": "Removes all anime girls from your game."
+        "NoPortraits": "Remove all anime girls from your game."
     }
 }
