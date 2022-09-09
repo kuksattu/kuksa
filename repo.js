0b20c47bc52641858474f46cdd2890c4594a497c
kuksa <kuksattu@gmail.com>
2022-09-09 10:29:29 +0300

    Fixing and added files.js

--- repo.js
+++ repo.js
@@ -1,12 +1,12 @@
 {
-    "contact": "kuksattu@gmail.com, koponenkt#8073",
-    "id": "kuksa",
-    "title": "Kuksa's patch repository",
-    "servers": [
-        // "https://mirrors.thpatch.net/kuksa/"
-        "https://raw.githubusercontent.com/kuksattu/kuksa/master/"
-    ],
-    "patches": {
-        "NoPortraits": "Remove all anime girls from your game."
-    }
+	"contact": "kuksattu@gmail.com, koponenkt#8073",
+	"id": "kuksa",
+	"patches": {
+		"NoPortraits": "Remove all anime girls from your game.",
+		"Ran": "Ran player character"
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/kuksattu/kuksa/master/"
+	],
+	"title": "Kuksa's patch repository"
 }
