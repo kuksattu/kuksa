4e72bab1999637d49c230328be6a5b6ca54e1008
kuksa <kuksattu@gmail.com>
2022-09-09 10:52:59 +0300

    Removed ran.

--- repo.js
+++ repo.js
@@ -2,8 +2,7 @@
 	"contact": "kuksattu@gmail.com, koponenkt#8073",
 	"id": "kuksa",
 	"patches": {
-		"NoPortraits": "Remove all anime girls from your game.",
-		"Ran": "Ran player character"
+		"NoPortraits": "Remove all anime girls from your game."
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/kuksattu/kuksa/master/"
