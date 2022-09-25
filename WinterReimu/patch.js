47c7c90a774bef8f07b9e74b164bf7c1104c82cb
kuksa <kuksattu@gmail.com>
2022-09-25 21:42:10 +0300

    Added WinterReimu patch

---
+++ WinterReimu/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "WinterReimu",
+	"servers": [
+		"https://raw.githubusercontent.com/kuksattu/kuksa/master/WinterReimu/"
+	],
+	"title": "Give the shrine maiden something warm to wear."
+}
--- repo.js
+++ repo.js
@@ -2,7 +2,8 @@
 	"contact": "kuksattu@gmail.com, koponenkt#8073",
 	"id": "kuksa",
 	"patches": {
-		"NoPortraits": "Remove all anime girls from your game."
+		"NoPortraits": "Remove all anime girls from your game.",
+		"WinterReimu": "Give the shrine maiden something warm to wear."
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/kuksattu/kuksa/master/"
