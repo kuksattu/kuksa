c47b8d8a652a5931a0f287c8446f476522d64789
kuksa <kuksattu@gmail.com>
2022-09-26 13:26:21 +0300

    Added the Reimu sprite to all mainline games

--- WinterReimu/files.js
+++ WinterReimu/files.js
@@ -1,4 +1,17 @@
 {
 	"patch.js": 155783108,
+	"th06/player00.png": 3830677396,
+	"th07/data/player00/player00.png": 2540275018,
+	"th07/player00.anm": 3313072455,
+	"th08/data/player00/player00.png": 1313257567,
+	"th09/data/pl/pl00/pl00.png": 287798065,
+	"th10/player/pl00/pl00.png": 287798065,
+	"th11/player/pl00/pl00.png": 287798065,
+	"th12/player/pl00/pl00.png": 287798065,
+	"th13/player/pl00/pl00.png": 287798065,
+	"th14/player/pl00/pl00.png": 287798065,
+	"th15/player/pl00/pl00.png": 287798065,
+	"th16/player/pl00/pl00.png": 287798065,
+	"th17/player/pl00/pl00.png": 287798065,
 	"th18/player/pl00/pl00.png": 1356197322
 }
